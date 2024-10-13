a14520c3ef597a6ced7638e3a7d22acac60d2332
aparasolTree <likeparasoltree@163.com>
2024-10-13 20:21:14 +0800

    v1.0.6

--- doc/islands.ts
+++ doc/islands.ts
@@ -1,7 +1,7 @@
 import { App } from "fresh";
 import { walk } from "@std/fs/walk";
 import { ComponentType } from "preact";
-import { join } from "@std/path";
+import { fromFileUrl, join } from "@std/path";
 
 const IS_LAND = /^_island/;
 export const ExampleComponents = {} as Record<string, ComponentType>;
@@ -12,7 +12,7 @@ export async function addIslands<State>(app: App<State>) {
         const filePath = new URL(join("..", path), import.meta.url);
         const map = await import(filePath.toString());
         for (const key of Object.keys(map)) {
-            app.island(filePath, key, map[key]);
+            app.island(fromFileUrl(filePath.toString()), key, map[key]);
             ExampleComponents[key] = map[key];
         }
     }

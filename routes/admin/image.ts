6ef994abebdb13f840c292d1f4b415dfc25f4bcb
aparasolTree <likeparasoltree@163.com>
2024-11-12 17:06:59 +0800

    v1.2.7

--- routes/admin/image.ts
+++ routes/admin/image.ts
@@ -1,11 +1,12 @@
 import { define } from "../../utils/define.ts";
 import { formDataVerify } from "../../utils/formDataVerify.ts";
 import { imageDataCache } from "../../utils/image-data-cache.ts";
+import { imageKvCache } from "../../utils/kv-cache.ts";
 import { deleteImages, getImageEntryByName } from "../../utils/kv/image.kv.ts";
 import { badRequest, json } from "../../utils/response.ts";
 
 export const handler = define.handlers({
-    async POST({ req, url }) {
+    async POST({ req }) {
         const [error, { name }] = formDataVerify(await req.formData(), {
             name: { type: "String", required: true },
         });
@@ -15,6 +16,7 @@ export const handler = define.handlers({
         if (!imageEntry) return badRequest("图片不存在");
         await deleteImages([imageEntry.id]);
         await imageDataCache.clear();
+        imageKvCache.clear();
         return json({ ok: true });
     },
 });
